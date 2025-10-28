a568e01d9ee2bdfc850b75a3abe7bc8d57583503
robrosk <rob.roskowski@gmail.com>
2025-10-28 16:48:39 -0400

    Error checking

--- Layers.py
+++ Layers.py
@@ -11,12 +11,11 @@ class Layer(ABC):
         raise NotImplementedError("Subclasses must implement forward()")
 
 class DenseLayer(Layer):
-    def __init__(self, input_size, output_size, activation_function):
-        self.weights = 0.01 * np.random.randn(output_size, input_size)
-        # R in output_size x input_size
-        self.biases = np.zeros((output_size, 1))
-        self.activation_function = activation_function
+    def __init__(self, input_size, output_size, activation_function: ActivationFunction):
+        self.weights = 0.01 * np.random.randn(output_size, input_size) # W in output_size x input_size
+        self.biases = np.zeros((output_size, 1)) # b in output_size x 1
+        self.activation_function = activation_function 
         
-    def forward(self, inputs):
+    def forward(self, inputs: np.ndarray) -> np.ndarray:
         return self.activation_function.forward(np.dot(self.weights, inputs) + self.biases)
         
