2940f5f3c5dfd8ae35bce5871d7688c7c675693c
robrosk <rob.roskowski@gmail.com>
2025-10-28 17:14:10 -0400

    More robust example of NN. Added in basics of backprop.

--- LossFunctions.py
+++ LossFunctions.py
@@ -6,16 +6,13 @@ class LossFunction(ABC):
         pass
     
     @abstractmethod
-    def loss(self, y_true, y_pred):
+    def calculate_loss(self, y_true, y_pred):
         raise NotImplementedError("Subclasses must implement loss()")
     
-    def forward(self, y_true, y_pred):
-        return self.loss(y_true, y_pred)
-    
 class MeanSquaredError(LossFunction):
-    def loss(self, y_true, y_pred):
+    def calculate_loss(self, y_true, y_pred):
         return np.mean(np.square(y_true - y_pred))
     
 class CrossEntropyLoss(LossFunction):
-    def loss(self, y_true, y_pred):
+    def calculate_loss(self, y_true, y_pred):
         return -np.sum(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,31 +1,55 @@
 from Layers import Layer, DenseLayer
 from ActivationFunctions import ReLU, Softmax
+from LossFunctions import LossFunction, MeanSquaredError
 import numpy as np
 
-class NeuralNetwork:
-    def __init__(self, layers: list[Layer]):
-        self.layers = layers
+LEARNING_RATE = 0.01
 
-    def forward_propagate(self, x: np.ndarray) -> np.ndarray:
+class NeuralNetwork:
+    def __init__(self, layers: list[Layer], loss_function: LossFunction):
+        self.layers = layers
+        self.loss_function = loss_function
+
+    def forward_prop(self, x: np.ndarray) -> np.ndarray:
         for layer in self.layers:
             x = layer.forward(x)
 
         return x
 
+    def calculate_loss(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return self.loss_function.forward(y_true, y_pred)
+
+    def backward_prop(self, loss: float):
+        pass
+
+    def train(self, x: np.ndarray, y_true: np.ndarray, learning_rate: float, epochs: int):
+        for epoch in range(epochs):
+            y_pred = self.forward_prop(x)
+            loss = self.loss_function.calculate_loss(y_true, y_pred)
+            self.backward_prop(loss)
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        return self.forward_prop(x).T[0]
 
 def main():
     nn = NeuralNetwork([
-        DenseLayer(input_size=8, output_size=4, activation_function=ReLU()),
-        DenseLayer(input_size=4, output_size=2, activation_function=Softmax())
-    ])
+        DenseLayer(input_size=64, output_size=32, activation_function=ReLU()),
+        DenseLayer(input_size=32, output_size=16, activation_function=ReLU()),
+        DenseLayer(input_size=16, output_size=8, activation_function=ReLU()),
+        DenseLayer(input_size=8, output_size=3, activation_function=Softmax())
+    ], loss_function=MeanSquaredError())
     
     print(nn.layers[0].weights.shape)
     print(nn.layers[1].weights.shape)
 
-    x = np.random.randn(8, 5)  # 5 examples, 8 features each
+    x = np.random.randn(64, 30)  # 30 examples, 64 features each
     print(x.shape)
-    output = nn.forward_propagate(x)
+    output = nn.forward_prop(x).T
     print(output)
+
+    x = np.random.randn(64, 1) # 1 example (what we want to predict based on our features), 64 features
+    output = nn.predict(x)
+    print(f"\n\nPrediction:\n{output}")
 
 if __name__ == "__main__":
     main()
